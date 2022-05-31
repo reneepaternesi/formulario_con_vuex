d3f6019cb5e5744fcda1974aa14192479eef55d7
renee.paternesi@coderhouse.com <renee.paternesi@coderhouse.com>
2022-05-31 15:59:36 -0300

    desafio formlario con vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import VueRouter from 'vue-router';
+import router from './router/router.js';
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
